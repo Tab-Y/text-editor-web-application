f8ba6bfa559f12243fc6893c8a84b752f6a9dfb8
Tab-Y <tlyoumans@gmail.com>
2022-12-02 14:01:56 -0500

    update database.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,35 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  console.log("making an update to the database boss.");
+
+  const theDb = await openDB('jate', 1);
+
+  const trans = theDb.transaction('jate', 'readwrite');
+
+  const store =  trans.objectStore('jate');
+
+  const request = store.add({ stuffToBeAdded });
+
+  const result = await request;
+  console.log("Things occured, and stuff got stored in the Db...", result)
+}
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  console.log('Were getting some data from the base. Be right back.');
+
+  const theBase = await openDB('jate', 1);
+
+  const trans = theBase.transaction('jate', 'readonly');
+
+  const store =  trans.objectStore('jate');
+
+  const request = store.add({ stuffToBeAdded });
+
+  const result = await request;
+  console.log("We're back from our journey. Here is the data", result)
+}
 
 initdb();
